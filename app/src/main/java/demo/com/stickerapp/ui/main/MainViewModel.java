f2aafbdea14cb95c689707e9fa991e8107bc825a
pabloyokese <jpo_54@hotmail.com>
2018-08-20 10:10:51 +0800

    add missing styles, colors

--- app/src/main/java/demo/com/stickerapp/ui/main/MainViewModel.java
+++ app/src/main/java/demo/com/stickerapp/ui/main/MainViewModel.java
@@ -1,9 +1,17 @@
 package demo.com.stickerapp.ui.main;
 
 
+import android.databinding.ObservableField;
+
 import demo.com.stickerapp.ui.base.BaseViewModel;
 
 public class MainViewModel extends BaseViewModel<MainNavigator> {
+
+    private final ObservableField<String> appVersion = new ObservableField<>();
+    public ObservableField<String> getAppVersion() {
+        return appVersion;
+    }
+
 
 
 }
